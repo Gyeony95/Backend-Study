f493e6d517c29db0920c8bc0dc99ea868a37fa5f
Gihyeon-Kwon <rlg1133@naver.com>
2021-09-25 15:23:04 +0900

    feat : 다국어 처리

--- SpringProjects/spring-start/src/main/java/com/example/springstart/SpringStartApplication.java
+++ SpringProjects/spring-start/src/main/java/com/example/springstart/SpringStartApplication.java
@@ -1,13 +1,26 @@
 package com.example.springstart;
 
+import java.util.Locale;
+
+import org.apache.tomcat.util.descriptor.LocalResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class SpringStartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStartApplication.class, args);
+	}
+	
+	@Bean //스프링 부트 초기화시 적용
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
 	}
 
 }
